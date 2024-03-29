079268465caa44aaddbe4d5ba0cb0dc27196350b
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-29 23:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function checkVote(name, age) {
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+		if (age>18) {
+			resolve(`Welcome, ${name}. You can vote.`)
+		} else {
+			reject(`Oh sorry ${name}. You aren't old enough.`)
+		}
+		}, 4000);
+	})
+}
+const handleSubmit = (e) => {
+	e.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = document.getElementById("age").value;
+	checkVote(name, age).then((data)=>{alert(data)}).catch((err)=>{alert(err)})
+}
